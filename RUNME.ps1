e64c528c91db93a362bd7ccff9ab564d4c9d829a
Luke Baughan <luke.baughan@gmail.com>
2023-11-25 18:57:18 +0000

    Aspire Project Wire-Up

--- RUNME.ps1
+++ RUNME.ps1
@@ -1,7 +1,8 @@
-param([String]$aspireAppHostFolder, [String]$serviceDefaultsPackage)
+param([String]$aspireSolutionFolder, [String]$serviceDefaultsPackage)
 
 # Welcome 
 Clear-Host
+$ProjectName = Split-Path -Path (Get-Location) -Leaf
 
 # Set location for required executables
 $DotNetExecutablePath = "C:\Program Files\dotnet\dotnet.exe"
@@ -24,3 +25,10 @@ Set-Location $ApiProjectFolder
 Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "add", ".\WebApi.csproj", "package", $serviceDefaultsPackage
 
 # Setup Service in Aspire Host 
+
+Set-Location $aspireSolutionFolder
+
+# dotnet sln add mylib1\mylib1.csproj --solution-folder mylibs
+
+Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "sln","add", "$UserInterfaceServerProjectFolder\UserInterface.csproj", "--solution-folder", "Services\$ProjectName\UserInterface"
+Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "sln","add", "$ApiProjectFolder\WebApi.csproj", "--solution-folder", "Services\$ProjectName\WebApi"
