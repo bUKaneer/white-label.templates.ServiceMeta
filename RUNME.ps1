96559aead9daa9e188cc2caee366e8da3e913628
Luke Baughan <luke.baughan@gmail.com>
2023-11-29 20:14:48 +0000

    Script fix

--- RUNME.ps1
+++ RUNME.ps1
@@ -1,4 +1,4 @@
-param([String]$aspireProjectName = "WhiteLabel.Aspire", [String]$aspireSolutionFolder = "C:\WhiteLabel\WhiteLabel\WhiteLabel.Aspire", [String]$packagesAndContainersSolutionFolder = "C:\WhiteLabel\WhiteLabel\WhiteLabel.Projects\WhiteLabel.PackagesAndContainers")
+param([String]$aspireProjectName = "WhiteLabel.Aspire", [String]$aspireSolutionFolder = "C:\WhiteLabel\WhiteLabel\WhiteLabel.Aspire", [String]$packagesAndContainersSolutionFolder = "C:\WhiteLabel\WhiteLabel\WhiteLabel.PackagesAndContainers")
 
 # Welcome 
 Clear-Host
@@ -15,6 +15,23 @@ $ApiProjectFolder = "$SolutionRootFolder\src\Application\$($ProjectName).WebApi"
 $DomainProjectFolder = "$SolutionRootFolder\src\$($ProjectName).Domain"
 $InfrastructureProjectFolder = "$SolutionRootFolder\src\$($ProjectName).Infrastructure"
 $UseCasesProjectFolder = "$SolutionRootFolder\src\$($ProjectName).UseCases"
+
+# Add Nuget Configuration so Service Defaults are findble
+
+$NugetConfigFilePath = "$aspireSolutionFolder\$aspireProjectName.AppHost\nuget.config"
+
+Copy-Item -Path $NugetConfigFilePath -Destination $UserInterfaceServerProjectFolder
+Copy-Item -Path $NugetConfigFilePath -Destination $ApiProjectFolder
+
+# Add Service Defaults Reference to User Interface and Api 
+
+Set-Location $UserInterfaceServerProjectFolder 
+
+Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "add", ".\$($ProjectName).UserInterface.csproj", "package", $serviceDefaultsPackage
+
+Set-Location $ApiProjectFolder
+
+Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "add", ".\$($ProjectName).WebApi.csproj", "package", $serviceDefaultsPackage
 
 # Pack and Push Service Projects to Baget
 
@@ -33,23 +50,6 @@ Start-Process -NoNewWindow -Wait $DotNetExecutablePath -ArgumentList "nuget", "p
 Set-Location $UseCasesProjectFolder
 Start-Process -NoNewWindow -Wait $DotNetExecutablePath -ArgumentList "pack", "--output nupkgs"
 Start-Process -NoNewWindow -Wait $DotNetExecutablePath -ArgumentList "nuget", "push", "./nupkgs/$ProjectName.UseCases.1.0.0.nupkg", "-s http://localhost:$PackageSourcePort/v3/index.json", "-k 8B516EDB-7523-476E-AF43-79CCA054CE9F"
-
-# Add Nuget Configuration so Service Defaults are findble
-
-$NugetConfigFilePath = "$aspireSolutionFolder\$aspireProjectName.AppHost\nuget.config"
-
-Copy-Item -Path $NugetConfigFilePath -Destination $UserInterfaceServerProjectFolder
-Copy-Item -Path $NugetConfigFilePath -Destination $ApiProjectFolder
-
-# Add Service Defaults Reference to User Interface and Api 
-
-Set-Location $UserInterfaceServerProjectFolder 
-
-Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "add", ".\$($ProjectName).UserInterface.csproj", "package", $serviceDefaultsPackage
-
-Set-Location $ApiProjectFolder
-
-Start-Process -NoNewWindow $DotNetExecutablePath -ArgumentList "add", ".\$($ProjectName).WebApi.csproj", "package", $serviceDefaultsPackage
 
 # Setup Service in Aspire Host 
 
@@ -80,5 +80,3 @@ Start-Process -Wait -NoNewWindow $DotNetExecutablePath -ArgumentList "add", "ref
 Start-Process -Wait -NoNewWindow $DotNetExecutablePath -ArgumentList "add", "reference", $UserInterfaceClientProjectFilePath
 
 Start-Process -Wait -NoNewWindow $DotNetExecutablePath -ArgumentList "add", "reference", $WebApiProjectFilePath
-
-
